7926ed02cab2bbb3dea65034aa9d494b54a53035
birudeogarande <birudeogarande@gmaill.com>
2024-07-14 18:38:13 +0530

    added myModule

--- practice/main.py
+++ practice/main.py
@@ -1,4 +1,14 @@
+from test.myModule import MyModule
+from test.myModule import person1
 from Person import Person
 
-person = Person("Birudeo", "Solapur")
+name = 'Birudeo'
+address = "Solapur"
+
+mymodule_instance = MyModule(name)
+mymodule_instance.greeting()
+
+person = Person(name, address)
 person.display()
+
+print(person1)
---
+++ practice/test/myModule.py
@@ -0,0 +1,14 @@
+class MyModule:
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
+    def greeting(self):
+        print(f"Hello, {self.name}")
+
+
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
